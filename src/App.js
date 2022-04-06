d649a6bb4022c3632da79369877e053bba957d85
Deniz <deniz.karakoc0104@gmail.com>
2022-04-06 11:06:35 +0200

    hallo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Was geeeeht :D
-          Du kekkkk
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Döner schmeckt</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>whaddup</h1>
+      <NavigationBar/>
     </div>
   );
 }
