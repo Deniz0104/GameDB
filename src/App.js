c8511d048c38acd2b8cdf9017fe70f1564f754c2
Deniz <deniz.karakoc0104@gmail.com>
2022-04-06 11:26:20 +0200

    components update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import NavigationBar from './components/NavigationBar';
+import SideBar from './components/SideBar';
+import Tilelist from './components/Tilelist';
 
 function App() {
   return (
     <div className="App">
-      <h1>whaddup</h1>
+      <div className='nav'>
       <NavigationBar/>
+      </div>
+      <div className='sideBar'>
+      <SideBar/>
+      </div>
+      <div className='header'>
+      <tileBar/>
+      </div>
+      <div className='content'>
+      <Tilelist/>
+      </div>
     </div>
   );
 }
---
+++ src/components/SideBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import styles from './SideBar.module.css'
+
+export default class SideBar extends Component {
+    render() {
+        return (
+            <h1>Test nsd</h1>
+        )
+    }
+}
