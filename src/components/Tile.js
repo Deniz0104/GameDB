492e50ac3ac8aaf9ca6c8956e493a5e5153e79a6
Deniz <deniz.karakoc0104@gmail.com>
2022-04-20 11:29:23 +0200

    list all the games with the fetch function in Tilelist

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react'
 import styles from "./Tile.module.css";
 
 export default class Tile extends Component {
+
+
+    constructor(props) {
+        super(props)
+    }
     render() {
         return (
-            <div className={styles.tileBox}> 
-                Game 
+
+            <div className={styles.tileBox} id="APIOutput"> 
+                 <h1>{this.props.name}</h1>
             </div>
         )
     }
