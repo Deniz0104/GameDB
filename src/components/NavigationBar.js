2c8dbfbc735e0e7c7160dc7f6232ff54df8298c6
Patrick-Loser <p_loser_Austria@gmx.at>
2022-04-20 11:30:22 +0200

    Navigation Bar almost complete

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,13 +3,43 @@ import styles from "./NavigationBar.module.css";
 
 export default class NavigationBar extends Component {
   constructor(props) {
-      super(props);
+    super(props);
     this.state = {
-      textBarValue: "Test",
+      textBarValue: "",
+      textBarLastUpdate: Date.now(),
+      showText: "",
     };
+    this.timeInterval = 300;
   }
   updateTextBarValue = (target) => {
-    this.setState({ textBarValue: target.value });
+    this.setState({
+      textBarValue: target.value,
+      textBarLastUpdate: Date.now(),
+    });
+    setTimeout(() => {
+      this.searchSuggestGames();
+    }, this.timeInterval);
+  };
+  searchSuggestGames = () => {
+    if (
+      this.state.textBarLastUpdate < Date.now() - (this.timeInterval - 50) &&
+      this.state.textBarLastUpdate > Date.now() - (this.timeInterval + 50) &&
+      this.state.textBarValue != ""
+    ) {
+      const options = {
+        method: "GET",
+      };
+      fetch(
+        "https://api.rawg.io/api/games?page_size=6&key=07eaf5a4bce8434b85cf5c1f9f03a302&search="+this.state.textBarValue,
+        options
+      )
+        .then((responsev) => responsev.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+      this.setState({
+        showText: this.state.textBarValue,
+      });
+    }
   };
 
   render() {
@@ -21,7 +51,7 @@ export default class NavigationBar extends Component {
           value={this.state.textBarValue}
           onChange={(target) => this.updateTextBarValue(target.target)}
         />
-        <h1>{this.state.textBarValue}</h1>
+        <h1>{this.state.showText}</h1>
       </div>
     );
   }
