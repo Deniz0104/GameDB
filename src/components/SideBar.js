d66257bbab5f2282341c51d1b2574b03f0945c61
Deniz <deniz.karakoc0104@gmail.com>
2022-04-28 11:40:01 +0200

    tile update

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,7 +5,9 @@ export default class SideBar extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <h1 className={styles.header}>Test nsd</h1>
+        <h1 className={styles.header}>Home</h1>
+        <h1 className={styles.header}>All Games</h1>
+        <h1 className={styles.header}>Top</h1>
       </div>
     );
   }
