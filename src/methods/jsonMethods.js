bbf356ff1a0a772aa8094a7de0acac690f903f27
Patrick Loser <p_loser_Austria@gmx.at>
2022-04-29 17:34:30 +0200

    fixing naming issue

--- src/methods/jsonMethods.js
+++ src/methods/jsonMethods.js
@@ -1,23 +1,41 @@
+import jsonData from "../data/platform.json";
+
 export async function fetchJson(method, url, target, parent) {
-    console.log(parent.state);
-  let result = await returnResult(method,url)
-  parent.setState({result:result})
-  console.log("Res: " + result + "  "+parent.state);
+  let result = await returnResult(method, url);
+  parent.setState({ result: result ,searching:false});
   return result;
 }
-async function returnResult(method, url){
-    let result;
-    const options = {
-        method: method,
-      };
-      try{
-      let response = await fetch(
-        url + "&key=07eaf5a4bce8434b85cf5c1f9f03a302",
-        options
-      );
-      result = await response.json();
-      }catch (err){
-          console.log(err);
-      }
-      return await result;
+async function returnResult(method, url) {
+  let result;
+  const options = {
+    method: method,
+  };
+  try {
+    let response = await fetch(
+      url + "&key=07eaf5a4bce8434b85cf5c1f9f03a302",
+      options
+    );
+    result = await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+  return await result;
+}
+
+
+
+export function returnPicture(searchedPlatforms){
+  let html = "";
+  let searching = [];
+  console.log(searchedPlatforms)
+  searchedPlatforms.forEach(element => {searching.push(element.platform.slug)})
+  console.log("array: "+ searching)
+  jsonData.platforms.forEach(element => {
+    if(searching.includes(element.slug)){
+      html += element.svg;
+      //console.log("Element: "+element.svg)
+    }
+  });
+  console.log("HTML: "+html);
+  return html
 }
