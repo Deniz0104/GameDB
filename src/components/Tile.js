ec7a595cbfbd4cc311f3c24ad71d5f9081ac887a
Deniz <deniz.karakoc0104@gmail.com>
2022-05-04 11:48:05 +0200

    tile designing

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import styles from "./Tile.module.css";
+import { returnPicture } from "../methods/jsonMethods";
 
 export default class Tile extends Component {
     render() {
         return (
             <div className={styles.tileBox}>
-                <div className={styles.imageArea}>
-                    <img src={this.props.background}></img>
-                </div>
+                <div
+                    className={styles.image}
+                    style={{
+                        backgroundImage: `url(${this.props.background})`,
+                    }}
+                />
+
                 <div className={styles.tileContent}>
-                    <div className={styles.metaScore}>{this.props.metacritic}</div>
+                    <div
+                        className={styles.svg}
+                        style={{ display: "flex" }}
+                        dangerouslySetInnerHTML={{
+                            __html: returnPicture(this.props.platforms)
+                        }}
+                    />
+                    {this.props.metacritic !== null ? <div className={styles.metaScore}>{this.props.metacritic}</div> : ""}
                     <h1>{this.props.name}</h1>
                 </div>
             </div>
