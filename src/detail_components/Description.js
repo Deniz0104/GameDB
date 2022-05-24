3f4e9f9e14d3e84d4ebbb37321ec93b209fbdcd9
Patrick-Loser <p_loser_Austria@gmx.at>
2022-05-24 16:53:22 +0200

    redesign description

--- src/detail_components/Description.js
+++ src/detail_components/Description.js
@@ -4,9 +4,10 @@ import styles from "./Description.module.css";
 export default function Description(props) {
   return (
     <div className={styles.container}>
-    <div
-      
-      dangerouslySetInnerHTML={{ __html: props.description }}
-    ></div></div>
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: props.description }}
+      ></div>
+    </div>
   );
 }
